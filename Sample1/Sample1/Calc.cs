bf6c430b2c21c127cbdd371325f5461f18f2bb33
Lóránt Szabó <lorant.szabo@semilab.hu>
2018-10-10 16:16:20 +0200

    function Div

--- Sample1/Sample1/Calc.cs
+++ Sample1/Sample1/Calc.cs
@@ -12,6 +12,9 @@ namespace Sample1
         {
             return a + b;
         }
-        
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
     }
 }
