86d3ed45b4efc624117146d6dfe88a62e351b1f9
Lóránt Szabó <lorant.szabo@semilab.hu>
2018-10-11 10:54:06 +0200

    Sub added

--- Sample1/Sample1/Calc.cs
+++ Sample1/Sample1/Calc.cs
@@ -12,6 +12,10 @@ namespace Sample1
         {
             return a + b;
         }
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
         public double Div(double a, double b)
         {
             return a / b;
